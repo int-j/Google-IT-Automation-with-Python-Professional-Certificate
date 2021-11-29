c61a182678732b431a5ca326d7a612e77a520a45
Maryem Bamou <34491538+int-j@users.noreply.github.com>
2021-11-29 15:48:11 +0100

    Update program.py

--- Using Python to Interact with the Operating System/program.py
+++ Using Python to Interact with the Operating System/program.py
@@ -1,3 +1,7 @@
+"""The create_python_script function creates a new python script in the current working directory,
+adds the line of comments to it declared 
+by the 'comments' variable, and returns the size of the new file. Fill in the gaps to create a script called "program.py". """
+
 def create_python_script(filename):
   comments = "# Start of a new Python program"
   with open( "program.py", "w")as f:
